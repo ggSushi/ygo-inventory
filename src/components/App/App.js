323d42d8bb3c64ea9f7203126eccdc3eef5a3583
Glonel Dimapilis <gdimapilis@aol.com>
2023-08-03 01:40:02 -0500

    shows all components on the DOM as is.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Inventory from '../Inventory/Inventory.jsx';
+import CardSearch from '../CardSearch/CardSearch.jsx';
+import ConceptDecks from '../ConceptDecks/ConceptDecks.jsx';
+import MyDecks from '../MyDecks/MyDecks.jsx';
 
 
 function App() {
   return (
     <div className="App-div">
-      Hello World
+      <h1>Hello World</h1>
+      <Inventory />
+      <CardSearch />
+      <MyDecks />
+      <ConceptDecks />
     </div>
   );
 }
