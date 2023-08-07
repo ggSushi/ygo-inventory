b956d827557116ade67e3af396d027da6385c6ba
Glonel Dimapilis <gdimapilis@aol.com>
2023-08-06 23:02:46 -0500

    added an if else to determine the color of the card.type searched. fixed an issue where it continued to rerender by putting the statement inside of a useEffect.

--- src/components/CardSearch/CardItem.jsx
+++ src/components/CardSearch/CardItem.jsx
@@ -3,12 +3,38 @@ import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 function CardItem({ card }) {
+  const [cardColor, setCardColor] = useState('#FFE09E')
 
+  useEffect(() => {
+    if (card.type === 'Normal Monster') {
+      setCardColor('#FFE09E');
+    } else if (card.type === 'Fusion Monster') {
+      setCardColor('#7972AE');
+    } else if (card.type === 'Synchro Monster') {
+      setCardColor('#E7E4DF');
+    } else if (card.type === 'Ritual Monster') {
+      setCardColor('#8AA2CB')
+    } else if (card.type === 'Link Monster') {
+      setCardColor('#1A4687')
+    } else if (card.type === 'Xyz Monster') {
+      setCardColor('#292D2D')
+    } else if (card.type === 'Pendulum Monster') {
+      setCardColor('#95A299')
+    } else if (card.type === 'Spell Card') {
+      setCardColor('#208685')
+    } else if (card.type === 'Trap Card') {
+      setCardColor('#D187A8')
+    } else {
+      setCardColor('transparent')
+    }
+  }, [])
 
   return (
     <div>
-      Card Data:
-      {card.id}
+      <ul>
+        <li>Name: {card.name}</li>
+        <li>Type: {card.type}</li>
+      </ul>
     </div>
   )
 }
