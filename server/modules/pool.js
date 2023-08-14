f912691eeb879af250fc60c683536cbf87c88ef8
Glonel Dimapilis <gdimapilis@aol.com>
2023-08-14 00:42:18 -0500

    wrote a POST query for inventory to store card info into db, and tested WORKING in Postman.

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -3,7 +3,7 @@ const pg = require('pg');
 const pool = new pg.Pool({
   host: 'localhost',
   port: 5432,
-  database: 'ygo-inventory'
+  database: 'ygo_inventory_db'
 });
 
 module.exports = pool;
--- server/routes/inventory.router.js
+++ server/routes/inventory.router.js
@@ -6,7 +6,37 @@ const pool = require('../modules/pool.js');
 
 
 // TODO POST Requests
+inventoryRouter.post('/addToInv', (req, res) => {
+  console.log(`POST to Inventory`);
+  const queryText = `Insert Into "inventory" 
+  ("card_name", "card_id", "card_type", "frameType", "desc", 
+  "atk", "def", "level", "type", "attribute", "card_sets", 
+  "card_images", "quantity", "storage_location")
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14);`
 
+  pool.query(queryText, [
+    req.body.name, 
+    req.body.id, 
+    req.body.type, 
+    req.body.frameType, 
+    req.body.desc, 
+    req.body.atk, 
+    req.body.def, 
+    req.body.level, 
+    req.body.race,
+    req.body.attribute,
+    req.body.card_sets,
+    req.body.card_images,
+    req.body.quantity,
+    req.body.storage_location
+  ])
+  .then((result) => {
+    res.sendStatus(200);
+  }).catch((error) => {
+    console.log(`Error in POST inventory: ${error}`);
+    res.sendStatus(500);
+  })
+});
 
 // TODO PUT Requests
 
